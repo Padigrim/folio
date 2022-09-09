fa52e2c568d619f4988f309e2e5c71dae8a3e8fa
Padigrim <36322443+Padigrim@users.noreply.github.com>
2022-09-09 02:18:32 -0400

    Patient Zero Testing
    Adding the first few files to check if this works

---
+++ js/loader.js
@@ -0,0 +1,7 @@
+var animation1 = bodymovin.loadAnimation({
+    container: document.getElementById('loaderAnim'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'searchLoader.json'
+});
