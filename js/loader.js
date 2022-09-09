25ce7aab82dfe03d18e764d6339722d3334e3518
Padigrim <36322443+Padigrim@users.noreply.github.com>
2022-09-09 02:52:05 -0400

    Test #2

--- js/loader.js
+++ js/loader.js
@@ -5,3 +5,15 @@ var animation1 = bodymovin.loadAnimation({
     autoplay: true,
     path: 'searchLoader.json'
 });
+
+var loader = document.getElementById('loader')
+
+window.addEventListener('load', (event) => {
+    setTimeout (function() {
+        loader.style.transition = '.5s';
+        loader.style.opacity = '0';
+        setTimeout (function() {
+            loader.style.display = "none";
+        }, 500);
+    }, 5000);
+});
